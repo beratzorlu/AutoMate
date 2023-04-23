62f8ed22371361fade54e362132771332ce54b42
berat.zorlu <berat.zorlu@hotmail.com>
2023-04-23 21:19:55 +0000

    Add superuser, post model and views.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
