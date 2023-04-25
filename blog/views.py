cb7d2810e15120e994a7083e788daa351ddefc73
berat.zorlu <berat.zorlu@hotmail.com>
2023-04-25 08:25:22 +0000

    Add view for post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views import generic
+from django.views import generic, View
 
 
 class PostList(generic.ListView):
@@ -13,6 +13,26 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 
-class DetailView(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+class DetailView(View):
+    """
+    Class-based view of the detailed post views
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by['-created_on']
+        is_liked = False
+        if post.likes.filter(self.request.user.id).exists():
+            is_liked = True
+        model = Post
+        template_name = 'post_detail.html'
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "is_liked": is_liked,
+            },
+        )
