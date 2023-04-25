db224180351cca795b76c1ca2046a2485d33ef73
berat.zorlu <berat.zorlu@hotmail.com>
2023-04-25 17:16:01 +0000

    Add detailed post and sidebar.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name="home"),
-    path('<slug:slug>/', views.PostList.as_view(), name="post_detail"),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.DetailView.as_view(), name='post_detail'),
 ]
