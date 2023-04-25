1d5292e51ad4aa14c362f71d510cc0fba8dd04d3
berat.zorlu <berat.zorlu@hotmail.com>
2023-04-25 00:01:55 +0000

    Add post display automation for frontend.

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,13 @@ from django.views import generic
 
 
 class PostList(generic.ListView):
+    """
+    Class-based view of the Post model
+    """
+    model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 6
 
 
 class DetailView(generic.DetailView):
