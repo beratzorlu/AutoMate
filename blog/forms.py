11c99d53f14d9b4e536b8c5522019e194061ab74
berat.zorlu <berat.zorlu@hotmail.com>
2023-04-26 22:32:28 +0000

    Fix comment textfield bug.

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from .models import Comment
+from crispy_forms.helper import FormHelper
 from django import forms
 
 
@@ -6,6 +7,11 @@ class UserCommentForm(forms.ModelForm):
     """
     Defines the model for the comment form
     """
+    def __init__(self, *args, **kwargs):
+        super(UserCommentForm, self).__init__(*args, **kwargs)
+        self.fields['body'].label = False
+
     class Meta:
         model = Comment
         fields = ('body', )
+
