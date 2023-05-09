4a1c461a80db9b1f37a92ebb07910360da9bdf16
berat.zorlu <berat.zorlu@hotmail.com>
2023-05-09 16:06:55 +0000

    Add default phone number format to phone field.

--- consultation/admin.py
+++ consultation/admin.py
@@ -1,26 +1,15 @@
 from django.contrib import admin
 from django.forms import ModelForm
 from .models import Consultation
-from phonenumber_field.widgets import PhoneNumberPrefixWidget
-
-# Register your models here.
-
-
-class PhoneFieldForm(ModelForm):
-    class Meta:
-        widgets = {
-            'phone': PhoneNumberPrefixWidget(),
-        }
 
 
 @admin.register(Consultation)
 class AdminConsultation(admin.ModelAdmin):
     actions = ['application_approval']
     prepopulated_fields = {'slug': ('first_name', 'last_name')}
-    list_display = ('first_name', 'last_name', 'email', 'birthdate', 'fav_maker', 'budget', 'purpose', 'created_on', 'updated_on', 'status', 'approved')
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'birthdate', 'fav_maker', 'budget', 'purpose', 'created_on', 'updated_on', 'status', 'approved')
     list_filter = ('created_on', 'approved', 'email', 'first_name', 'last_name')
     search_fields = ('first_name', 'last_name', 'email', 'status')
-    form = PhoneFieldForm
 
     def application_approval(self, request, queryset):
         queryset.update(approved=True)
