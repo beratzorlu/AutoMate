d3c4a453f08ff3fe80e64db853c302c867fde409
berat.zorlu <berat.zorlu@hotmail.com>
2023-05-11 01:47:56 +0000

    Update search and filter list content.

--- consultation/admin.py
+++ consultation/admin.py
@@ -5,10 +5,7 @@ from .models import Consultation
 
 @admin.register(Consultation)
 class AdminConsultation(admin.ModelAdmin):
-    actions = ['application_approval']
     list_display = ('author', 'first_name', 'last_name', 'phone', 'birthdate', 'fav_maker', 'budget', 'purpose', 'created_on', 'updated_on', 'status',)
-    list_filter = ('created_on', 'first_name', 'last_name')
-    search_fields = ('first_name', 'last_name', 'status')
+    list_filter = ('author', 'created_on', 'status')
+    search_fields = ('first_name', 'last_name', 'author__username')
 
-    def application_approval(self, request, queryset):
-        queryset.update(approved=True)
