7b054286b982038cca277428d6955ce95a201ac8
berat.zorlu <berat.zorlu@hotmail.com>
2023-05-13 15:05:06 +0000

    Fix blog page responsivity and update post model.

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    excerpt = models.TextField(blank=True)
+    excerpt = models.TextField(max_length=100, blank=True)
     status = models.IntegerField(choices=STATUS, default=1)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts')
