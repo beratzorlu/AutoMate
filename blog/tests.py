591b91228ce8c4e0f32571b0ae794baf8addd062
berat.zorlu <berat.zorlu@hotmail.com>
2023-05-13 17:55:36 +0000

    Add unittests for post and comment forms.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .forms import UserCommentForm, UserAddPostForm, UserPostEditForm
 
-# Create your tests here.
+
+class TestUserCommentForm(TestCase):
+    def test_post_user_content_input_is_required(self):
+        """
+        Tests if the required restriction for comment form behaves as expected.
+        """
+        form = UserCommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+
+class TestUserAddPostForm(TestCase):
+    def test_post_user_content_input_is_required(self):
+        """
+        Tests if the required restriction for add post form behaves as expected.
+        """
+        form = UserCommentForm({'content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
